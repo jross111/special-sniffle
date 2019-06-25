f91901d62f8015ef187491b3608d5dea4a59f346
jross111 <jross.nyc111@gmail.com>
2019-06-25 14:22:16 -0400

    all links up, name props for articles

---
+++ blog/src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <React.Fragment>
+    <h1>About Me</h1>
+    <p>
+        Stuff about me.
+    </p>
+   
+    </React.Fragment>
+);
+
+
+export default AboutPage;
---
+++ blog/src/pages/ArticlePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ArticlePage = ({ match }) => {
+    const name = match.params.name
+    return (
+        <React.Fragment>
+            <h1>This is the {name}.</h1>
+
+
+        </React.Fragment>
+    );
+}
+
+export default ArticlePage;
