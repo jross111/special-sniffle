c39829abb953f26780cef9872515526c46cc4d1e
jross111 <jross.nyc111@gmail.com>
2019-08-15 19:25:10 -0400

    AddCommentForm component structured

---
+++ blog/src/components/AddCommentForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+
+
+const AddCommentForm = () => {
+    const [username, setUsername] = useState('');
+    const [commentText, setCommentText] = useState('');
+
+    return (
+        <div id="add-comment-form">
+            <h3>Add a Comment</h3>
+            <label>
+                Name:
+            <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+            </label>
+
+            <label>
+                Comment:
+            <textarea rows="4" cols="50" value={commentText} onChange={(event) => setCommentText(event.target.value)} />
+            </label>
+
+            <button>Add Comment</button>
+        </div>
+    )
+}
