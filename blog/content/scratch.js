577af7357c5074c8e6c5a7b9ee3a9866733c29a2
jross111 <jross.nyc111@gmail.com>
2019-09-14 16:16:06 -0400

    json lock security issue resolved

--- blog/content/scratch.js
+++ blog/content/scratch.js
@@ -1,13 +1,19 @@
+function abbrevName(name){
+  let names = name.toUpperCase().split(' ')
+  return names[0][0] + "." + names[1][0]
 
-
-function counter(max){
-arr = [];
-for (let i = 0; i < max ; i++) {
-  arr.push(i);
 }
-console.log(arr)
+console.log(abbrevName("jamie ross"))
+
+console.log(abbrevName("am Harris"), "S.H");
+console.log(abbrevName("Patrick Feenan"), "P.F");
+console.log(abbrevName("Evan Cole"), "E.C");
+console.log(abbrevName("P Favuzzi"), "P.F");
+console.log(abbrevName("David Mendieta"), "D.M");
+
+
+
+const abbrevName(name) => {
+    let names = name.toUpperCase().split(' ')
+  return names[0][0] + "." + names[1][0]
 }
-
-
-counter(100);  
-
